dcfce9c976d9c80012a7a5ed5880be796c018e5d
Romanyuk Artem <wrdxwrdxwrdx@gmail.com>
2023-09-13 18:44:20 +0300

    add practice tasks solutions

---
+++ src/practice/practice_1/task_2.py
@@ -0,0 +1,14 @@
+def array_shift(array, m):
+    array = array[::-1]
+    n = len(array) - m
+    array[:n] = array[n-1::-1]
+    array[n:] = array[len(array):n-1:-1]
+
+    return array
+
+
+if __name__ == '__main__':
+    input_array = input("Введите массив через пробел: ")
+    array = list(map(int, input_array.split()))
+    m = int(input('Введите число m: '))
+    print(f'Результат: {array_shift(array, m)}')
