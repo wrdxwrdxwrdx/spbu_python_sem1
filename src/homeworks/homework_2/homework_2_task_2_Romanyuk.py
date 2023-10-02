4cd5851f4751387e0f7d324026990772a7f04be4
Romanyuk Artem <wrdxwrdxwrdx@gmail.com>
2023-10-02 21:27:59 +0300

    edit with black

--- src/homeworks/homework_2/homework_2_task_2_Romanyuk.py
+++ src/homeworks/homework_2/homework_2_task_2_Romanyuk.py
@@ -5,7 +5,7 @@ from os.path import exists
 def create_word_list(file_name):
     word_counter = {}
 
-    with open(file_name, 'r') as file:
+    with open(file_name, "r") as file:
         for line in file:
             for word in line.split():
                 if word in word_counter.keys():
@@ -17,20 +17,20 @@ def create_word_list(file_name):
 
 
 def write_in_file(write_file, word_counter):
-    with open(write_file, 'w') as file:
+    with open(write_file, "w") as file:
         writer = csv.writer(file)
         for word in word_counter:
             writer.writerow([word, word_counter[word]])
 
 
-if __name__ == '__main__':
-    read_file = input('input file [src].txt: ')
-    write_file = input('output file [dst].csv: ')
+if __name__ == "__main__":
+    read_file = input("input file [src].txt: ")
+    write_file = input("output file [dst].csv: ")
     if exists(read_file):
         word_counter = create_word_list(read_file)
 
         write_in_file(write_file, word_counter)
 
-        print(f'{write_file} done')
+        print(f"{write_file} done")
     else:
-        print(f'{read_file} does not exist')
+        print(f"{read_file} does not exist")
