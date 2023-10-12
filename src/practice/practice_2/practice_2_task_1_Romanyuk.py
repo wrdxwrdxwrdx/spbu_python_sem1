a3bf3385dc2b21cba75b04133176aa13eb53a695
wrdxwrdxwrdx <wrdxwrdxwrdx@gmail.com>
2023-10-12 19:21:51 +0300

    practice_2_task_1_Romanyuk v2.0

--- src/practice/practice_2/practice_2_task_1_Romanyuk.py
+++ src/practice/practice_2/practice_2_task_1_Romanyuk.py
@@ -1,3 +1,13 @@
+def gcf(a, b):
+    a, b = sorted([a, b])
+    if a == 1 or b == 1:
+        return 1
+    elif a == 0:
+        return b
+    else:
+        return gcf(a, b % a)
+
+
 def get_fractions(max_denominator):
     answer = []
     for denominator in range(2, max_denominator + 1):
@@ -5,7 +15,7 @@ def get_fractions(max_denominator):
             if numerator == 1:
                 answer.append((numerator, denominator))
 
-            elif denominator % numerator != 0:
+            elif gcf(denominator, numerator) == 1:
                 answer.append((numerator, denominator))
 
     answer.sort(key=lambda fraction: fraction[0] / fraction[1])
