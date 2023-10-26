02203e129fb1648b78be2361b260ec69199f7dbd
wrdxwrdxwrdx <wrdxwrdxwrdx@gmail.com>
2023-10-26 23:49:49 +0300

    Initial commit

---
+++ src/homeworks/homework_5/homework_5_task_1.py
@@ -0,0 +1,25 @@
+INPUT_TEXT = "Введите строку для кодировки в UTF-16:\n"
+
+
+def get_unicode(char: str) -> str:
+    unicode = hex(ord(char))[2:].upper()
+    return f"U+{('0'*(4-len(unicode)) + unicode)}"
+
+
+def get_unicode_binary(char: str) -> str:
+    binary_unicode = bin(ord(char))[2:]
+    answer = "0" * (16 - len(binary_unicode)) + binary_unicode
+    return answer[:8] + " " + answer[8:]
+
+
+def main():
+    user_input = input(INPUT_TEXT)
+
+    for char in user_input:
+        unicode = get_unicode(char)
+        unicode_binary = get_unicode_binary(char)
+        print("{} {} {}".format(char, unicode, unicode_binary))
+
+
+if __name__ == "__main__":
+    main()
---
+++ tests/homeworks/homework_5/test_task_1.py
@@ -0,0 +1,73 @@
+from src.homeworks.homework_5.homework_5_task_1 import *
+import pytest
+from io import StringIO
+
+
+@pytest.mark.parametrize(
+    "char,expected", [("H", "U+0048"), ("e", "U+0065"), (",", "U+002C")]
+)
+def test_get_unicode(char, expected):
+    assert get_unicode(char) == expected
+
+
+@pytest.mark.parametrize(
+    "char,expected",
+    [
+        ("H", "00000000 01001000"),
+        ("e", "00000000 01100101"),
+        (",", "00000000 00101100"),
+    ],
+)
+def test_get_unicode_binary(char, expected):
+    assert get_unicode_binary(char) == expected
+
+
+@pytest.mark.parametrize(
+    "user_input,expected",
+    [
+        (
+            "Hello, World!",
+            [
+                "H U+0048 00000000 01001000",
+                "e U+0065 00000000 01100101",
+                "l U+006C 00000000 01101100",
+                "l U+006C 00000000 01101100",
+                "o U+006F 00000000 01101111",
+                ", U+002C 00000000 00101100",
+                "  U+0020 00000000 00100000",
+                "W U+0057 00000000 01010111",
+                "o U+006F 00000000 01101111",
+                "r U+0072 00000000 01110010",
+                "l U+006C 00000000 01101100",
+                "d U+0064 00000000 01100100",
+                "! U+0021 00000000 00100001",
+                "",
+            ],
+        ),
+        (
+            "Lorem Ipsum",
+            [
+                "L U+004C 00000000 01001100",
+                "o U+006F 00000000 01101111",
+                "r U+0072 00000000 01110010",
+                "e U+0065 00000000 01100101",
+                "m U+006D 00000000 01101101",
+                "  U+0020 00000000 00100000",
+                "I U+0049 00000000 01001001",
+                "p U+0070 00000000 01110000",
+                "s U+0073 00000000 01110011",
+                "u U+0075 00000000 01110101",
+                "m U+006D 00000000 01101101",
+                "",
+            ],
+        ),
+        ("😀😁", ["😀 U+1F600 11111011 000000000", "😁 U+1F601 11111011 000000001", ""]),
+    ],
+)
+def test_main_scenario(monkeypatch, user_input, expected):
+    monkeypatch.setattr("builtins.input", lambda _: user_input)
+    fake_output = StringIO()
+    monkeypatch.setattr("sys.stdout", fake_output)
+    main()
+    output = fake_output.getvalue()
+    assert output.split("\n") == expected
