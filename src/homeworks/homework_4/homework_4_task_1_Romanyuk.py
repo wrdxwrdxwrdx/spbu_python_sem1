1c13f2e6f6ccf0de37aa86d3dd1ac190b2e34174
wrdxwrdxwrdx <wrdxwrdxwrdx@gmail.com>
2023-11-10 00:16:14 +0300

    fix: binary -1 problem

--- src/homeworks/homework_4/homework_4_task_1_Romanyuk.py
+++ src/homeworks/homework_4/homework_4_task_1_Romanyuk.py
@@ -11,69 +11,64 @@ def get_size_bytes(number):
 
 def binary_addition(num_1, num_2):
     extra_number = 0
-    answer = []
+    output = []
 
-    for i in range(len(num_1) - 1, -1, -1):
-        sum_digit = extra_number + num_1[i] + num_2[i]
+    for i in range(1, len(num_1) + 1):
+        sum_digit = extra_number + num_1[-i] + num_2[-i]
 
         if sum_digit > 1:
             extra_number = 1
         else:
             extra_number = 0
-        answer.insert(0, sum_digit % 2)
-
-    if len(answer) > len(num_1):
-        return normalize_binary(answer[1:], len(answer[1:]))
-    return normalize_binary(answer, len(answer))
+        output.append(sum_digit % 2)
+    output = output[::-1]
+    if len(output) > len(num_1):
+        return normalize_binary(output[1:], len(output[1:]))
+    return normalize_binary(output, len(output))
 
 
 def denary_to_binary(number):
-    is_minus = number < 0
-    answer = []
-    number = abs(number)
+    output = []
+    abs_number = abs(number)
 
-    while number >= 1:
-        answer.insert(0, number % 2)
-        number //= 2
-
-    binary_one = normalize_binary([0, 1], len(answer))
-
-    if is_minus:
-        return [1] + binary_addition(inverse_number(answer), binary_one)
-    return [0] + answer
+    while abs_number >= 1:
+        output.append(abs_number % 2)
+        abs_number //= 2
+    output = output[::-1]
+    binary_one = normalize_binary([0, 1], len(output))
+    if number < 0:
+        return [1] + binary_addition(inverse_number(output), binary_one)
+    return [0] + output
 
 
 def binary_to_denary(number):
-    answer = 0
-    is_minus = number.pop(0)
-    if is_minus:
-        number = binary_addition(
-            inverse_number(number), normalize_binary([0, 1], len(number))
-        )[::-1]
-    else:
-        number = number[::-1]
-    for i in range(len(number)):
-        answer += number[i] * (2**i)
-    return (-1) ** is_minus * answer
+    output = 0
+    for i in range(1, len(number) + 1):
+        output += number[-i] * 2 ** (i - 1)
+
+        if i == len(number):
+            output -= number[-i] * 2**i
+    return output
 
 
 def inverse_number(number):
-    return list(map(lambda x: int(not x), number))
+    return list(map(lambda a: 0 if a else 1, number))
 
 
 def binary_subtraction(num_1, num_2):
-    num_2 = binary_addition(inverse_number(num_2), normalize_binary([0, 1], len(num_2)))
-    bytes = max(get_size_bytes(num_1), get_size_bytes(num_2))
+    output = binary_addition(
+        inverse_number(num_2), normalize_binary([0, 1], len(num_2))
+    )
+    bytes = max(get_size_bytes(num_1), get_size_bytes(output))
     return binary_addition(
-        normalize_binary(num_1, bytes * 8), normalize_binary(num_2, bytes * 8)
+        normalize_binary(num_1, bytes * 8), normalize_binary(output, bytes * 8)
     )
 
 
-def arr_to_str(arr):
-    return "".join(map(str, arr))
-
-
 def main():
+    def arr_to_str(arr):
+        return "".join(map(str, arr))
+
     num_1 = int(input("Первое число: "))
     num_2 = int(input("Второе число: "))
     num_1_bin, num_2_bin = denary_to_binary(num_1), denary_to_binary(num_2)
