c58e5ce4420257ab7ecbc091c80ba18a75b04693
Romanyuk Artem <117483924+wrdxwrdxwrdx@users.noreply.github.com>
2023-11-11 11:08:40 +0300

    practice_8_task_1_Romanyuk (#26)
    * Initial commit
    
    * fix: keys_number
    
    * fix: change hash function

---
+++ src/practice/practice_8/practice_8_task_1/hash_table.py
@@ -0,0 +1,104 @@
+from dataclasses import dataclass, field
+from typing import TypeVar, Optional, Generic
+
+Key = TypeVar("Key")
+Value = TypeVar("Value")
+
+
+@dataclass
+class HashTable(Generic[Value]):
+    values: list[Optional[Value]] = field(
+        default_factory=lambda: [[] for _ in range(16)]
+    )
+    keys_number: int = 0
+    capacity: int = 16
+
+
+def hash_function(key: Key, capacity: int) -> int:
+    return hash(key) % capacity
+
+
+def get_load_factor(hash_table: HashTable) -> float:
+    load_factor = hash_table.keys_number / hash_table.capacity
+    return load_factor
+
+
+def resize(hash_table: HashTable) -> None:
+    capacity = hash_table.capacity
+    new_capacity = capacity * 2
+    values = hash_table.values
+
+    hash_table.values = [[] for _ in range(new_capacity)]
+    hash_table.capacity = new_capacity
+    hash_table.keys_number = 0
+    for hash in range(capacity):
+        for key_value in values[hash]:
+            if key_value:
+                put(hash_table, *key_value)
+
+
+def create_hash_table() -> HashTable:
+    return HashTable()
+
+
+def delete_hash_table(hash_table: HashTable) -> None:
+    for hash in range(hash_table.capacity):
+        for _ in range(len(hash_table.values[hash])):
+            del hash_table.values[hash][0]
+
+
+def put(hash_table: HashTable, key: Key, value: Value) -> None:
+    if get_load_factor(hash_table) <= 0.8:
+        hash = hash_function(key, hash_table.capacity)
+        for index, key_value in enumerate(hash_table.values[hash]):
+            if key_value[0] == key:
+                hash_table.values[hash][index] = (key, value)
+                break
+        else:
+            hash_table.values[hash].append((key, value))
+        hash_table.keys_number += 1
+
+    else:
+        resize(hash_table)
+        put(hash_table, key, value)
+
+
+def remove(hash_table: HashTable, key: Key) -> Value:
+    hash = hash_function(key, hash_table.capacity)
+    for index, key_value in enumerate(hash_table.values[hash]):
+        if key_value[0] == key:
+            value = key_value[1]
+            del hash_table.values[hash][index]
+            hash_table.keys_number -= 1
+            return value
+    else:
+        raise ValueError("Такого ключа нет в hash table")
+
+
+def get(hash_table: HashTable, key: Key) -> Value:
+    hash = hash_function(key, hash_table.capacity)
+
+    for key_value in hash_table.values[hash]:
+        if key_value[0] == key:
+            return key_value[1]
+    else:
+        raise ValueError("Такого ключа нет в hash table")
+
+
+def has_key(hash_table: HashTable, key: Key) -> bool:
+    hash = hash_function(key, hash_table.capacity)
+
+    for key_value in hash_table.values[hash]:
+        if key_value[0] == key:
+            return True
+    return False
+
+
+def items(hash_table: HashTable) -> list[tuple[Key, Value]]:
+    output = []
+
+    for hash in range(hash_table.capacity):
+        for key_value in hash_table.values[hash]:
+            output.append(key_value)
+
+    return output
---
+++ tests/practice/practice_8/test_practice_8_task_1.py
@@ -0,0 +1,165 @@
+import pytest
+
+from src.practice.practice_8.practice_8_task_1.hash_table import *
+
+
+def dummy_hash_table(elements: tuple):
+    hash_table = create_hash_table()
+    for i in elements:
+        put(hash_table, i[0], i[1])
+    return hash_table
+
+
+def test_create_hash_table():
+    has_table = create_hash_table()
+    assert isinstance(has_table, HashTable)
+
+
+@pytest.mark.parametrize(
+    "elements,expected",
+    [
+        ((("may 1", 1), ("may 2", 2), ("may 3", 3), ("may 4", 4)), 4 / 16),
+        ((("may 1", 1), ("may 2", 2), ("may 3", 3)), 3 / 16),
+        (
+            (("may 1", 1), ("may 2", 2), ("may 3", 3), ("may 4", 4), ("may 5", 5)),
+            5 / 16,
+        ),
+        ((), 0),
+    ],
+)
+def test_get_load_factor(elements, expected):
+    hash_table = dummy_hash_table(elements)
+    assert get_load_factor(hash_table) == expected
+
+
+@pytest.mark.parametrize(
+    "elements,expected",
+    [
+        ((("may 1", 1), ("may 2", 2), ("may 3", 3), ("may 4", 4)), 32),
+        ((("may 1", 1), ("may 2", 2), ("may 3", 3), ("may 4", 4)), 32),
+        ((("may 1", 1), ("may 2", 2), ("may 3", 3), ("may 4", 4)), 32),
+        (
+            (
+                ("may 1", 1),
+                ("may 2", 2),
+                ("may 3", 3),
+                ("may 4", 4),
+                ("may 5", 5),
+                ("may 6", 6),
+                ("may 7", 7),
+                ("may 8", 8),
+                ("may 9", 9),
+                ("may 10", 10),
+                ("may 11", 11),
+                ("may 12", 12),
+                ("may 13", 13),
+                ("may 14", 14),
+            ),
+            64,
+        ),
+    ],
+)
+def test_resize(elements, expected):
+    hash_table = dummy_hash_table(elements)
+    resize(hash_table)
+    assert hash_table.capacity == expected
+
+
+@pytest.mark.parametrize(
+    "elements,key,expected",
+    [
+        ((("may 1", 1), ("may 2", 2), ("may 3", 3), ("may 4", 4)), "may 4", 4),
+        ((("may 1", 1), ("may 2", 2), ("may 3", 3), ("may 4", 4)), "may 3", 3),
+        ((("may 1", 1), ("may 2", 2), ("may 3", 3), ("may 4", 4)), "may 1", 1),
+    ],
+)
+def test_remove(elements, key, expected):
+    hash_table = dummy_hash_table(elements)
+    keys_number = hash_table.keys_number
+    assert remove(hash_table, key) == expected and (
+        keys_number - hash_table.keys_number == 1
+    )
+
+
+@pytest.mark.parametrize(
+    "elements,key",
+    [
+        ((("may 1", 1), ("may 2", 2), ("may 3", 3), ("may 4", 4)), "may 123"),
+        ((("may 1", 1), ("may 2", 2), ("may 3", 3), ("may 4", 4)), "may 23"),
+        ((("may 1", 1), ("may 2", 2), ("may 3", 3), ("may 4", 4)), "may 5"),
+    ],
+)
+def test_error_remove(elements, key):
+    hash_table = dummy_hash_table(elements)
+    with pytest.raises(ValueError):
+        remove(hash_table, key)
+
+
+@pytest.mark.parametrize(
+    "elements,key,expected",
+    [
+        ((("may 1", 1), ("may 2", 2), ("may 3", 3), ("may 4", 4)), "may 4", 4),
+        ((("may 1", 1), ("may 2", 2), ("may 3", 3), ("may 4", 4)), "may 3", 3),
+        ((("may 1", 1), ("may 2", 2), ("may 3", 3), ("may 4", 4)), "may 1", 1),
+    ],
+)
+def test_get(elements, key, expected):
+    hash_table = dummy_hash_table(elements)
+    assert get(hash_table, key) == expected
+
+
+@pytest.mark.parametrize(
+    "elements,key",
+    [
+        ((("may 1", 1), ("may 2", 2), ("may 3", 3), ("may 4", 4)), "may 123"),
+        ((("may 1", 1), ("may 2", 2), ("may 3", 3), ("may 4", 4)), "may 23"),
+        ((("may 1", 1), ("may 2", 2), ("may 3", 3), ("may 4", 4)), "may 5"),
+    ],
+)
+def test_error_get(elements, key):
+    hash_table = dummy_hash_table(elements)
+    with pytest.raises(ValueError):
+        get(hash_table, key)
+
+
+@pytest.mark.parametrize(
+    "elements,key,expected",
+    [
+        ((("may 1", 1), ("may 2", 2), ("may 3", 3), ("may 4", 4)), "may 4", True),
+        ((("may 1", 1), ("may 2", 2), ("may 3", 3), ("may 4", 4)), "may 3", True),
+        ((("may 1", 1), ("may 2", 2), ("may 3", 3), ("may 4", 4)), "may 11", False),
+    ],
+)
+def test_has_key(elements, key, expected):
+    hash_table = dummy_hash_table(elements)
+    assert has_key(hash_table, key) == expected
+
+
+@pytest.mark.parametrize(
+    "elements,expected",
+    [
+        (
+            (("may 1", 1), ("may 2", 2), ("may 3", 3), ("may 4", 4)),
+            [("may 1", 1), ("may 2", 2), ("may 3", 3), ("may 4", 4)],
+        ),
+        ((("may 1", 1), ("may 2", 2)), [("may 1", 1), ("may 2", 2)]),
+        ((), []),
+    ],
+)
+def test_items(elements, expected):
+    hash_table = dummy_hash_table(elements)
+    assert sorted(items(hash_table)) == sorted(expected)
+
+
+@pytest.mark.parametrize(
+    "elements,expected",
+    [
+        ((("may 1", 1), ("may 2", 2), ("may 3", 3), ("may 4", 4)), False),
+        ((("may 1", 1), ("may 2", 2)), False),
+        ((), False),
+    ],
+)
+def test_get(elements, expected):
+    hash_table = dummy_hash_table(elements)
+    delete_hash_table(hash_table)
+    assert bool(items(hash_table)) == expected
