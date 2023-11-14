fd4e468e01556b29bed08c90f528ebaf254afc62
wrdxwrdxwrdx <wrdxwrdxwrdx@gmail.com>
2023-11-14 23:14:26 +0300

    fix: remove re functions

--- src/homeworks/homework_5/homework_5_task_2/dna_functions.py
+++ src/homeworks/homework_5/homework_5_task_2/dna_functions.py
@@ -1,8 +1,10 @@
-import re
+import string
 
 
-def is_valid_dna_code(dna_code: str) -> bool:
-    if re.search("[а-яА-я]", dna_code):
+def is_valid_dna_code(dna_code: str) -> None:
+    if not all(
+        map(lambda x: x in string.digits or x in string.ascii_letters, dna_code)
+    ):
         raise ValueError("Код днк должен содержать только буквы латиницы")
 
     if not dna_code[-1].isdigit():
@@ -11,45 +13,64 @@ def is_valid_dna_code(dna_code: str) -> bool:
     if dna_code[0].isdigit():
         raise ValueError("Код днк не должен начинаться с числа")
 
-    output = []
+    chars = (
+        "".join(list(map(lambda char: " " if char.isdigit() else char, dna_code)))
+    ).split()
+    numbers = (
+        "".join(list(map(lambda char: " " if char.isalpha() else char, dna_code)))
+    ).split()
 
-    def digit_check(array: list, match: re.Match) -> str:
-        array.append(dna_code[match.span()[0] - 1].isdigit())
-        return match[0]
+    if "0" in numbers:
+        raise ValueError("Код днк не должен содержать 0")
 
-    re.sub("[a-zA-z]", lambda x: digit_check(output, x), dna_code)
+    output = list(map(lambda char: len(char) == 1, chars))
 
-    def char_check(array: list, match: re.Match) -> str:
-        if match.group() == "0":
-            raise ValueError("Код днк не должен содержать 0")
-        array.append(dna_code[match.span()[0] - 1].isalpha())
-        return match[0]
-
-    re.sub("[0-9]+", lambda x: char_check(output, x), dna_code)
-
-    return all(output)
+    if not all(output):
+        raise ValueError("Код днк не должен содержать две буквы подряд")
 
 
-def is_valid_dna(dna: str) -> bool:
-    if re.search("[а-яА-я]", dna):
-        raise ValueError("Днк должен содержать только буквы латиницы")
+def is_valid_dna(dna: str) -> None:
+    if not all(map(lambda x: x in string.digits or x in string.ascii_letters, dna)):
+        raise ValueError("Код днк должен содержать только буквы латиницы")
 
-    return bool(re.match("^[a-zA-z]+$", dna))
+    numbers = (
+        "".join(list(map(lambda char: " " if char.isalpha() else char, dna)))
+    ).split()
+
+    if len(numbers) >= 1:
+        raise ValueError("В днк не должно быть цифр")
 
 
 def encode(dna: str) -> str:
-    if not is_valid_dna(dna):
-        raise ValueError("В днк не должно быть цифр")
+    is_valid_dna(dna)
 
-    output = re.sub(r"(\w)\1*", lambda x: f"{x[1]}{len(x[0])}", dna)
+    output = ""
+
+    start_pointer = 0
+    end_pointer = 0
+
+    while end_pointer < len(dna):
+        while end_pointer < len(dna) and dna[start_pointer] == dna[end_pointer]:
+            end_pointer += 1
+        output += f"{dna[start_pointer]}{end_pointer-start_pointer}"
+        start_pointer = end_pointer
 
     return output
 
 
 def decode(dna_code: str) -> str:
-    pattern = "[A-Za-z]+[0-9]*"
+    is_valid_dna_code(dna_code)
 
-    if not is_valid_dna_code(dna_code):
-        raise ValueError("Код днк не должен содержать две буквы подряд")
+    chars = (
+        "".join(list(map(lambda char: " " if char.isdigit() else char, dna_code)))
+    ).split()
 
-    return re.sub(pattern, lambda x: f"{x[0][0]*int(x[0][1:])}", dna_code)
+    numbers = (
+        "".join(list(map(lambda char: " " if char.isalpha() else char, dna_code)))
+    ).split()
+
+    output = "".join(
+        list(map(lambda index: chars[index] * int(numbers[index]), range(len(chars))))
+    )
+
+    return output
