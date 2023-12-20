7d6ea33eff6daaaafc2a330260b9c486fed33028
wrdxwrdxwrdx <wrdxwrdxwrdx@gmail.com>
2023-12-21 00:51:54 +0300

    fix: delete namedtouple

--- src/practice/practice_10/parser_module.py
+++ src/practice/practice_10/parser_module.py
@@ -14,99 +14,67 @@ class ParseTree:
     root: Optional[Node] = None
 
 
-Output = namedtuple("output", "pointer node")
-
-
 def pretty_print(tree: ParseTree):
     def pretty_print_node(space_number: int, node: Node):
+        print("." * space_number + node.name)
         for child in node.children:
-            print("." * (space_number + 4), child.name)
             pretty_print_node(space_number + 4, child)
 
     if tree.root:
-        print(tree.root.name)
         pretty_print_node(0, tree.root)
     else:
         raise ValueError("Tree is Empty")
 
 
-def _start(pointer: int, tokens: list[str]) -> Optional[Output]:
-    t_pointer, t_node = _t(pointer, tokens)
-    sum_pointer, sum_node = _sum(t_pointer, tokens)
-    return Output(
-        pointer=sum_pointer,
-        node=Node("START", [t_node, sum_node]),
-    )
+def _start(pointer: int, tokens: list[str]) -> (Node, int):
+    t_node, t_pointer = _t(pointer, tokens)
+    sum_node, sum_pointer = _sum(t_pointer, tokens)
+    return Node("START", [t_node, sum_node]), sum_pointer
 
 
-def _sum(pointer: int, tokens: list[str]) -> Optional[Output]:
+def _sum(pointer: int, tokens: list[str]) -> (Node, int):
     if pointer < len(tokens) and tokens[pointer] == "+":
-        if _t(pointer + 1, tokens):
-            t_pointer, t_node = _t(pointer + 1, tokens)
-            if _sum(t_pointer, tokens):
-                sum_pointer, sum_node = _sum(t_pointer, tokens)
-                return Output(
-                    pointer=sum_pointer,
-                    node=Node("SUM", [Node("+"), t_node, sum_node]),
-                )
+        t_node, t_pointer = _t(pointer + 1, tokens)
+        sum_node, sum_pointer = _sum(t_pointer, tokens)
+        return Node("SUM", [Node("+"), t_node, sum_node]), sum_pointer
     if pointer >= len(tokens) or tokens[pointer] != "+":
-        return Output(
-            pointer=pointer,
-            node=Node("SUM", [Node("eps")]),
-        )
+        return Node("SUM", [Node("eps")]), pointer
     raise ValueError(f"incorrect sum")
 
 
-def _t(pointer: int, tokens: list[str]) -> Optional[Output]:
-    token_pointer, token_node = _token(pointer, tokens)
-    prod_pointer, prod_node = _prod(token_pointer, tokens)
-    return Output(
-        pointer=prod_pointer,
-        node=Node("T", [token_node, prod_node]),
-    )
+def _t(pointer: int, tokens: list[str]) -> (Node, int):
+    token_node, token_pointer = _token(pointer, tokens)
+    prod_node, prod_pointer = _prod(token_pointer, tokens)
+    return Node("T", [token_node, prod_node]), prod_pointer
 
 
-def _prod(pointer: int, tokens: list[str]) -> Optional[Output]:
+def _prod(pointer: int, tokens: list[str]) -> (Node, int):
     if pointer < len(tokens) and tokens[pointer] == "*":
-        if _token(pointer + 1, tokens):
-            token_pointer, token_node = _token(pointer + 1, tokens)
-            if _prod(token_pointer, tokens):
-                prod_pointer, prod_node = _prod(token_pointer, tokens)
-                return Output(
-                    pointer=prod_pointer,
-                    node=Node("PROD", [Node("*"), token_node, prod_node]),
-                )
-        else:
-            raise ValueError(f"invalid character after '*', index {pointer+1}")
+        token_node, token_pointer = _token(pointer + 1, tokens)
+        prod_node, prod_pointer = _prod(token_pointer, tokens)
+        return Node("PROD", [Node("*"), token_node, prod_node]), prod_pointer
     if pointer >= len(tokens) or tokens[pointer] != "*":
-        return Output(
-            pointer=pointer,
-            node=Node("PROD", [Node("eps")]),
-        )
+        return Node("PROD", [Node("eps")]), pointer
     raise ValueError(f"incorrect prod")
 
 
-def _token(pointer: int, tokens: list[str]) -> Optional[Output]:
+def _token(pointer: int, tokens: list[str]) -> (Node, int):
     if pointer >= len(tokens):
         raise ValueError(f"incorrect mathematical expression")
     if pointer < len(tokens) and tokens[pointer] == "(":
-        if _start(pointer + 1, tokens):
-            start_pointer, start_node = _start(pointer + 1, tokens)
-            if start_pointer < len(tokens) and tokens[start_pointer] == ")":
-                return Output(
-                    pointer=start_pointer + 1,
-                    node=Node("TOKEN", [Node("("), start_node, Node(")")]),
-                )
+        start_node, start_pointer = _start(pointer + 1, tokens)
+        if start_pointer < len(tokens) and tokens[start_pointer] == ")":
+            return (
+                Node("TOKEN", [Node("("), start_node, Node(")")]),
+                start_pointer + 1,
+            )
     if tokens[pointer].isdigit():
-        return Output(
-            pointer=pointer + 1,
-            node=Node("TOKEN", [Node(f"id({tokens[pointer]})")]),
-        )
+        return Node("TOKEN", [Node(f"id({tokens[pointer]})")]), pointer + 1
     raise ValueError(f"incorrect token {tokens[pointer]}")
 
 
 def parse(tokens: list[str]) -> ParseTree:
     if tokens:
-        return ParseTree(_start(0, tokens).node)
+        return ParseTree(_start(0, tokens)[0])
     else:
         raise ValueError("Empty token list")
